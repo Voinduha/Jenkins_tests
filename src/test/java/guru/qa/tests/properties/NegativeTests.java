35fdd78524b59dad14d6d34fafead1e10e87d491
danvu <danvu@ya.ru>
2022-02-06 16:57:03 +0300

    телеграмм нотификэйшнс

---
+++ src/test/java/guru/qa/tests/properties/NegativeTests.java
@@ -0,0 +1,28 @@
+package guru.qa.tests.properties;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Tag("simple")
+public class NegativeTests {
+    @Test
+    void test00() {
+        assertTrue(false);
+    }
+
+    @Test
+    void test01() {
+        assertTrue(false);
+    }
+
+    @Test
+    void test02() {
+        assertTrue(false);
+    }
+
+    @Test
+    void test03() {
+        assertTrue(false);
+    }
+}
--- src/test/java/guru/qa/tests/properties/SystemPropertiesTests.java
+++ src/test/java/guru/qa/tests/properties/SystemPropertiesTests.java
@@ -5,18 +5,21 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+
 public class SystemPropertiesTests {
 
         //System.getProperty("key");
         //System.setProperty("key", "value");
 
     @Test
+    @Tag("simple")
     void someTest1() {
         String someValue = System.getProperty("someKey");
         System.out.println(someValue);
     }
 
     @Test
+    @Tag("simple")
     void someTest2() {
         System.setProperty("someKey", "red value");
         String someValue = System.getProperty("someKey");
@@ -24,12 +27,14 @@ public class SystemPropertiesTests {
     }
 
     @Test
+    @Tag("simple")
     void someTest3() {
         String someValue = System.getProperty("someKey", "blue value");
         System.out.println(someValue); // red value
     }
 
     @Test
+    @Tag("simple")
     void someTest4() {
         Boolean someValue = Boolean.parseBoolean(
              System.getProperty("someKey", "true"));
